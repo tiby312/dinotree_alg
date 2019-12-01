b88a188100c72e5f189951aef5fd36aaade662bc
Ken Reed <kenakioreed@gmail.com>
2019-11-30 22:13:17 -0500

    cleanup

--- src/pmut.rs
+++ src/pmut.rs
@@ -1,12 +1,36 @@
 //! PMut is short for protected mut reference.
 //! This is basically a pointer that restricts some things the user can do.
-//!  
 //!
+//! It prevents the user from violating the invariants of the tree.
+//!
+//! ```rust
+//! extern crate dinotree_alg;
+//! extern crate axgeom;
+//! use dinotree_alg::prelude::*;
+//!
+//!
+//! let mut bots=[BBox::new(axgeom::Rect::new(0,10,0,10),0)];
+//! let mut tree=DinoTree::new(&mut bots);
+//! 
+//! tree.find_collisions_mut(|mut a,mut b|{
+//!    //We cannot allow the user to swap these two
+//!    //bots. They should be allowed to mutate 
+//!    //whats inside each of them (aside from their aabb),
+//!    //but not swap.
+//!    
+//!    //core::mem::swap(a,b); // We cannot allow this!!!!
+//!
+//!    //This is allowed.  
+//!    core::mem::swap(a.inner_mut(),b.inner_mut());
+//! })
+//!
+//! ```
 
 use crate::inner_prelude::*;
 
 
-
+///A protected mutable reference. 
+///See the pmut module documentation for more explanation.
 pub struct PMut<'a,T:?Sized>{
     inner:&'a mut T
 }
@@ -17,7 +41,7 @@ impl<'a,T:?Sized> PMut<'a,T>{
     pub fn as_mut(&mut self)->PMut<T>{
         PMut{inner:self.inner}
     }
-    
+
     pub fn as_ref(&self)->&T{
         self.inner
     }
@@ -106,7 +130,7 @@ impl<'a, T> core::iter::IntoIterator for PMut<'a,[T]> {
 }
 
 
-///Iterator produced by `ProtectedBBoxSlice<T>` that generates `ProtectedBBox<T>`
+///Iterator produced by `PMut<[T]>` that generates `PMut<T>`
 pub struct PMutIter<'a,T>{
     inner:core::slice::IterMut<'a,T>
 }
@@ -133,201 +157,4 @@ impl<'a, T> DoubleEndedIterator for PMutIter<'a, T> {
         self.inner.next_back().map(|inner|PMut{inner})
     }
 }
-
-
-/*
-///Forbids the user from swapping two nodes around.
-#[repr(transparent)]
-pub struct ProtectedNode<'a,T>{
-    inner:&'a mut T
-}
-impl<'a,T:Node> ProtectedNode<'a,T>{
-    pub fn new(inner:&'a mut T)->Self{
-        ProtectedNode{inner}
-    }
-
-    pub fn get(self)->NodeRef<'a,T::T>{
-        self.inner.get()
-    }
-    pub fn get_mut(self)->NodeRefMut<'a,T::T>{
-        self.inner.get_mut()
-    }
-    pub fn as_ref(&mut self)->ProtectedNode<T>{
-        ProtectedNode{inner:self.inner}
-    }
-}
-
-
-///Forbids the user from swapping aabb's around.
-#[repr(transparent)]
-pub struct ProtectedBBox<'a,T>{
-    inner:&'a mut T
-}
-
-
-impl<'a,T> ProtectedBBox<'a,T>{
-    #[inline(always)]
-    pub fn as_mut(&mut self)->ProtectedBBox<T>{
-        ProtectedBBox{inner:self.inner}
-    }
-
-}
-
-
-
-
-
-unsafe impl<'a,T:Aabb> Aabb for ProtectedBBox<'a,T>{
-    type Num=T::Num;
-    #[inline(always)]
-    fn get(&self)->&Rect<Self::Num>{
-        self.inner.get()
-    }
-}
-impl<'a,T:HasInner> HasInner for ProtectedBBox<'a,T>{
-    type Inner=T::Inner;
-    #[inline(always)]
-    fn get_inner(&self)->(&Rect<T::Num>,&Self::Inner){
-        self.inner.get_inner()
-    }
-
-    #[inline(always)]
-    fn get_inner_mut(&mut self)->(&Rect<T::Num>,&mut Self::Inner){
-        self.inner.get_inner_mut()
-    }
-}
-
-
-
-
-impl<'a,T> core::borrow::Borrow<T> for ProtectedBBox<'a,T>{
-    #[inline(always)]
-    fn borrow(&self)->&T{
-        self.inner
-    }
-}
-
-impl<'a,T> AsRef<T> for ProtectedBBox<'a,T>{
-    #[inline(always)]
-    fn as_ref(&self)->&T{
-        self.inner
-    }
-}
-
-
-impl<'a,T> core::borrow::Borrow<[T]> for ProtectedBBoxSlice<'a,T>{
-    #[inline(always)]
-    fn borrow(&self)->&[T]{
-        self.inner
-    }
-}
-
-impl<'a, T> core::iter::IntoIterator for ProtectedBBoxSlice<'a,T> {
-    type Item = ProtectedBBox<'a,T>;
-    type IntoIter = ProtectedBBoxIter<'a,T>;
-
-    #[inline(always)]
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter_mut()
-    }
-}
-
-impl<'a,T> AsRef<[T]> for ProtectedBBoxSlice<'a,T>{
-    #[inline(always)]
-    fn as_ref(&self)->&[T]{
-        self.inner
-    }
-}
-
-
-///Forbids the user from swapping mutable slices in the nodes around.
-#[repr(transparent)]
-pub struct ProtectedBBoxSlice<'a,T>{
-    inner:&'a mut [T]
-}
-
-impl<'a,T> ProtectedBBoxSlice<'a,T>{
-    
-    #[inline(always)]
-    pub fn len(&self)->usize{
-        self.inner.len()
-    }
-
-    #[inline(always)]
-    pub fn is_empty(&self)->bool{
-        self.inner.is_empty()
-    }
-
-    #[inline(always)]
-    pub fn split_first_mut(self)->Option<(ProtectedBBox<'a,T>,ProtectedBBoxSlice<'a,T>)>{
-        self.inner.split_first_mut().map(|(first,inner)|(ProtectedBBox{inner:first},ProtectedBBoxSlice::new(inner)))
-    }
-
-
-    #[inline(always)]
-    pub fn truncate_to(self,a:core::ops::RangeTo<usize>)->Self{
-        ProtectedBBoxSlice{inner:&mut self.inner[a]}
-    }
-    #[inline(always)]
-    pub fn truncate_from(self,a:core::ops::RangeFrom<usize>)->Self{
-        ProtectedBBoxSlice{inner:&mut self.inner[a]} 
-    }
-
-
-    #[inline(always)]
-    pub fn truncate(self,a:core::ops::Range<usize>)->Self{
-        ProtectedBBoxSlice{inner:&mut self.inner[a]}
-    }
-
-    #[inline(always)]
-    pub fn as_mut(&mut self)->ProtectedBBoxSlice<T>{
-        ProtectedBBoxSlice{inner:self.inner}
-    }
-
-    #[inline(always)]
-    pub fn new(inner:&'a mut [T])->Self{
-        ProtectedBBoxSlice{inner}
-    }
-
-    #[inline(always)]
-    pub fn iter(self)->core::slice::Iter<'a,T>{
-        self.inner.iter()
-    }
-    #[inline(always)]
-    pub fn iter_mut(self)->ProtectedBBoxIter<'a,T>{
-        ProtectedBBoxIter{inner:self.inner.iter_mut()}
-    }
-}
-
-///Iterator produced by `ProtectedBBoxSlice<T>` that generates `ProtectedBBox<T>`
-pub struct ProtectedBBoxIter<'a,T>{
-    inner:core::slice::IterMut<'a,T>
-}
-impl<'a,T> Iterator for ProtectedBBoxIter<'a,T>{
-    type Item=ProtectedBBox<'a,T>;
-
-    #[inline(always)]
-    fn next(&mut self)->Option<ProtectedBBox<'a,T>>{
-        self.inner.next().map(|inner|ProtectedBBox{inner})
-    }
-
-    #[inline(always)]
-    fn size_hint(&self)->(usize,Option<usize>){
-        self.inner.size_hint()
-    }
-}
-
-impl<'a,T> core::iter::FusedIterator for ProtectedBBoxIter<'a,T>{}
-impl<'a,T> core::iter::ExactSizeIterator for ProtectedBBoxIter<'a,T>{}
-
-impl<'a, T> DoubleEndedIterator for ProtectedBBoxIter<'a, T> {
-    #[inline(always)]
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.inner.next_back().map(|inner|ProtectedBBox{inner})
-    }
-}
-
-*/
-
-
 
